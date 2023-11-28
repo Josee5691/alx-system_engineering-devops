ea1494cc41619cb45c8ab0af62d7bad8b4947c09
Joseph Kariuki <josephkariuki790@gmail.com>
2023-11-28 15:25:57 +0300

    fix

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(?P<sender>.*?\] \[to:<?P<reciever>.*?\] \[flags:<?P<flags>.*?\]/)
+puts ARGV[0].scan(/\[from:(?<sender>.*?)\] \[to:(?<reciever>.*?)\] \[flags:(?<flags>.*?)\]/)
