c7b70514f32285ff059054a3650f2ab05c21d5e7
Joseph Kariuki <josephkariuki790@gmail.com>
2024-03-06 10:22:22 +0300

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,43 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and returns the number of subscribers for a given subreddit.
-"""
-
+""" querry reddit api for subreddit info"""
 import requests
+import requests.auth
+
 
 def number_of_subscribers(subreddit):
-    """
-    Retrieves the number of subscribers for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit.
-        
-    Returns:
-        int: The number of subscribers for the subreddit, or 0 if the subreddit is invalid.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
+    """ querry reddit api"""
+    sub = subreddit
+    subreddit = "/r/{}/about".format(sub)
+    temp = "HolbertonPass845"
 
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+    secret = "Z4Sa9bA6RRE44qDyhHQiTlW1gd0"
+    client_id = "hy4KvoK0W2iDvw"
+    usr_name = "jgadelugo"
+
+    client_auth = requests.auth.HTTPBasicAuth(client_id, secret)
+    post_data = {"grant_type": "password",
+                 "username": usr_name,
+                 "password": temp}
+    headers = {"User-Agent": "ChangeMeClient/0.1 by {}".format(usr_name)}
+    response = requests.post("https://www.reddit.com/api/v1/access_token",
+                             auth=client_auth, data=post_data, headers=headers)
+    auth_json = response.json()
+
+    token_type = auth_json['token_type']
+    access_token = auth_json['access_token']
+
+    headers = {"Authorization": "{} {}".format(token_type, access_token),
+               "User-Agent": "ChangeMeClient/0.1 by {}".format(usr_name)}
+
+    query = "https://oauth.reddit.com{}".format(subreddit)
+    res = requests.get(query, headers=headers)
+
+    status = res.status_code
+
+    if (status != 200):
+        num_of_subs = 0
     else:
-        return 0
+        info = res.json()
+        num_of_subs = info["data"]["subscribers"]
 
+    return (int(num_of_subs))
